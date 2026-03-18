fc9c561e1ce7db164d2d36d8a48166f6b12a45f4
FernandoYepes <122755418+fernandoYepes@users.noreply.github.com>
2026-03-18 11:45:08 -0300

    Add files via upload

---
+++ Algoritmo e Estrutura de Dados/aula 1 - ListaEncadeada/aula 2 - continuacao aula 1 - Lista Encadeada/Lista.py
@@ -0,0 +1,59 @@
+from No import No
+
+class Lista:
+
+    def __init__(self):
+        self.inicio = None
+
+    def imprimir(self):
+        print("------------------")
+        print("Lista Encadeada por ordem de chegada")
+        if self.inicio is None:
+            print("Lista Vazia!")
+        else:
+            aux = self.inicio
+            while aux:
+                print( aux.dado )
+                aux = aux.prox
+
+    def add(self, valor):
+        nodo = No(valor)
+        if self.inicio == None:
+            self.inicio = nodo
+        else:
+            aux = self.inicio
+            while aux.prox:
+                aux = aux.prox
+            aux.prox = nodo
+        self.imprimir()
+
+    def remover(self, valor):
+        if self.inicio == None:
+            print("Lista Vazia!")
+        else:
+            removeu = False
+            if valor == self.inicio.dado:
+                self.inicio = self.inicio.prox
+                removeu = True
+            else:
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux:
+                    if valor == aux.dado:
+                        ant.prox = aux.prox
+                        removeu = True
+                        break
+                    else:
+                        ant = aux
+                        aux = aux.prox
+            if removeu:
+                print(valor, " removido!")
+            else:
+                print( valor , " não encontrado!")
+                
+            self.imprimir()
+            
+
+
+        
+        
---
+++ Algoritmo e Estrutura de Dados/aula 1 - ListaEncadeada/aula 2 - continuacao aula 1 - Lista Encadeada/main.py
@@ -0,0 +1,14 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+lista.add("João")
+lista.add("Carlos")
+lista.add("Fernando")
+lista.add("Talita")
+lista.add("Eduarda")
+
+
+lista.remover("Carlos")
+lista.remover("Júlia")
